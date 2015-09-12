eb61e45e070654c25b2192cc937e41aa69d7ae64
SimonOrJ <simonorj@outlook.com>
2015-09-11 23:59:22 -0400

    added settings.php
    Maybe more things to come, but these are the required basics.

---
+++ settings.php
@@ -0,0 +1,28 @@
+<?php
+/* --------------------------------
+    Database Settings for conn.php
+   --------------------------------*/
+// Hostname
+$dbhost = localhost
+// NySQL username
+$dbuser = username
+// MySQL password associated with username
+$dbpass = password
+// Database name
+$dbname = minecraft
+// CoreProtect prefix (if you have custom prefix) Default: co_
+$co_prefix = co_
+
+/* ----------------------------------
+    Auxiliary settings for index.php
+   ----------------------------------*/
+// Dynamic Map link (Optional, uncomment to activate)
+//$dynmapURL = http://localhost:8123/
+// More features to come...
+
+/* ---------------------------
+    End of user configuration
+   ---------------------------*/
+
+$mcsql = new mysqli($dbhost,$dbuser,$dbpass,$dbname);
+?>
