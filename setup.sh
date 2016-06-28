a52c826a952f0b274509fa68be885da87bc72640
Simon <simonorj@outlook.com>
2016-06-28 05:12:33 -0400

    Create setup.sh
    for easy installation

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#/bin/sh
+
+# Change write permissions
+chmod 777 cache/
+
+# Uh, that's basically it for now...
+# TODO: 1. Add configuration options
+# TODO: 2. Add setup .bat for Windows users
+# TODO: 3. Maybe make a website configuration option...
